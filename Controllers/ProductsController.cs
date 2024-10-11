a3744398f23070986c3042586cd27efd3db640b8
PochienTsai <bernie7135@gmail.com>
2024-10-11 13:53:12 +0800

    Add multiple HTML templates and CSS/JS for client-side actions
    Introduce various HTML templates for different purposes including Todo CRUD, product apps, and WebAPI integration. Also, add client-side activities through CSS and JavaScript for enhanced interactivity and design.

--- Controllers/ProductsController.cs
+++ Controllers/ProductsController.cs
@@ -2,90 +2,91 @@
 using Microsoft.AspNetCore.Mvc;
 //********************************************************
 using WebApplication2022_Core8_WebApi_JWT.Models;
-//********************************************************
 
+//********************************************************
+// 方法命名開頭加入Http verb : Get/Head/Post/Put/Delete/Connect/Options/Patch
 /// <summary>
 /// 檢視畫面（UI網頁），放在 /wwwroot目錄底下，名為 index_Products.html
 /// 類別檔，放在 /Models/Product.cs
 /// </summary>
-namespace WebApplication2022_Core8_WebApi_JWT.Controllers
+namespace WebApplication2022_Core8_WebApi_JWT.Controllers;
+
+[Route("api/[controller]")]
+[ApiController]
+public class ProductsController : ControllerBase
 {
-    [Route("api/[controller]")]
-    [ApiController]
-    public class ProductsController : ControllerBase
+    #region // 加入預設值（基本資料）
+
+    // 程式沒有防呆，ID數字請勿重複！
+    private Product[] products = new Product[]
     {
-        #region  // 加入預設值（基本資料）
-        // 程式沒有防呆，ID數字請勿重複！
-        Product[] products = new Product[]
-        {
-            new Product { Id = 1, Name = "Tomato Soup", Category = "Groceries", Price = 1 },
-            new Product { Id = 2, Name = "Yo-yo", Category = "Toys", Price = 3.75M },
-            new Product { Id = 3, Name = "Hammer", Category = "Hardware", Price = 16.99M },
-            new Product { Id = 4, Name = "張中文", Category = "Toys", Price = 5 },
-            new Product { Id = 5, Name = "李法文", Category = "Hardware", Price = 6M },
-            new Product { Id = 6, Name = "許功蓋(PHP)", Category = "Groceries", Price = 7 }
-        };
-        #endregion
+        new() { Id = 1, Name = "Tomato Soup", Category = "Groceries", Price = 1 },
+        new() { Id = 2, Name = "Yo-yo", Category = "Toys", Price = 3.75M },
+        new() { Id = 3, Name = "Hammer", Category = "Hardware", Price = 16.99M },
+        new() { Id = 4, Name = "張中文", Category = "Toys", Price = 5 },
+        new() { Id = 5, Name = "李法文", Category = "Hardware", Price = 6M },
+        new() { Id = 6, Name = "許功蓋(PHP)", Category = "Groceries", Price = 7 }
+    };
 
+    #endregion
 
-        // 沒有輸入值。列出全部的資料。
-        //[HttpGet]   // .NET Core嚴格要求，必填！
-        //public IEnumerable<Product> GetAllProducts()
-        //{   // 原廠範例    // 傳回值 -- (4) 其他類型
-        //    return products;  
-        //}
+    // 沒有輸入值。列出全部的資料。
+    //[HttpGet]   // .NET Core嚴格要求，必填！
+    //public IEnumerable<Product> GetAllProducts()
+    //{   // 原廠範例    // 傳回值 -- (4) 其他類型
+    //    return products;
+    //}
 
-        [HttpGet]   // .NET Core嚴格要求，必填！
-        public IActionResult GetAllProducts()
-        {   // 傳回值 -- (3)  IActionResult ( .NET Core的 WebAPI有變化）
-            return Ok(products);
-            // returns an HttpStatusCode.OK   // https://docs.microsoft.com/zh-tw/previous-versions/aspnet/dn308866%28v%3dvs.118%29
-        }
-
-        //===========================================================
-
-        // 需要一個輸入值。只列出「單一筆」資料。
+    [HttpGet] // .NET Core嚴格要求，必填！
+    public IActionResult GetAllProducts()
+    {
         // 傳回值 -- (3)  IActionResult ( .NET Core的 WebAPI有變化）
-        //[HttpGet("{id}")]   // .NET Core嚴格要求，必填！
-        //public IActionResult GetProduct(int id)
-        //{   // 原廠範例  // 傳回值 -- (3)  IHttpActionResult
-        //    var product = products.FirstOrDefault((p) => p.Id == id);
-        //    if (product == null)   {
-        //        return NotFound();  // 找不到。這也是 Http.Results的一種，需搭配 IHttpActionResult使用
-        //    }
-        //    return Ok(product);
-        //    // returns an HttpStatusCode.OK   // https://docs.microsoft.com/zh-tw/previous-versions/aspnet/dn308866%28v%3dvs.118%29
-        //}
+        return Ok(products);
+        // returns an HttpStatusCode.OK   // https://docs.microsoft.com/zh-tw/previous-versions/aspnet/dn308866%28v%3dvs.118%29
+    }
 
-        [HttpGet("{id}")]   // .NET Core嚴格要求，必填！
-        public Product GetProduct(int id)
+    //===========================================================
+
+    // 需要一個輸入值。只列出「單一筆」資料。
+    // 傳回值 -- (3)  IActionResult ( .NET Core的 WebAPI有變化）
+    //[HttpGet("{id}")]   // .NET Core嚴格要求，必填！
+    //public IActionResult GetProduct(int id)
+    //{   // 原廠範例  // 傳回值 -- (3)  IHttpActionResult
+    //    var product = products.FirstOrDefault((p) => p.Id == id);
+    //    if (product == null)   {
+    //        return NotFound();  // 找不到。這也是 Http.Results的一種，需搭配 IHttpActionResult使用
+    //    }
+    //    return Ok(product);
+    //    // returns an HttpStatusCode.OK   // https://docs.microsoft.com/zh-tw/previous-versions/aspnet/dn308866%28v%3dvs.118%29
+    //}
+
+    [HttpGet("{id}")] // .NET Core嚴格要求，必填！
+    public Product GetProduct(int id)
+    {
+        var pd = products.FirstOrDefault((p) => p.Id == id);
+        if (pd == null)
         {
-            var pd = products.FirstOrDefault((p) => p.Id == id);
-            if (pd == null)
+            var pdx = new Product()
             {
-                Product pdx = new Product()
-                {
-                    Name = "找不到",
-                    Price = 0
-                };
-                return pdx;
-            }
-            return pd;
+                Name = "找不到",
+                Price = 0
+            };
+            return pdx;
         }
 
-        //===========================================================
-        // https://docs.microsoft.com/zh-tw/aspnet/web-api/overview/getting-started-with-aspnet-web-api/action-results
-        // Web API 控制器動作可以傳回：
-        //  (1)  void -- 傳回 "空"。204 （沒有內容）
-        //  (2)  HttpResponseMessage -- 直接將轉換的 HTTP 回應訊息。
-        //
-        //  (3)  IActionResult -- 呼叫 .ExecuteAsync()來建立HttpResponseMessage，然後將轉換為 HTTP 回應訊息。
-        //         ( .NET Core的 WebAPI有變化）
-        //         
-        //  (4)  其他類型 -- 將 序列化 的傳回值寫入至回應主體中; 傳回 200 （確定）。
-        //                            缺點是您不能直接傳回錯誤碼 404 等。 不過，您可以擲回HttpResponseException錯誤代碼
-        //===========================================================
-
-
+        return pd;
     }
+
+    //===========================================================
+    // https://docs.microsoft.com/zh-tw/aspnet/web-api/overview/getting-started-with-aspnet-web-api/action-results
+    // Web API 控制器動作可以傳回：
+    //  (1)  void -- 傳回 "空"。204 （沒有內容）
+    //  (2)  HttpResponseMessage -- 直接將轉換的 HTTP 回應訊息。
+    //
+    //  (3)  IActionResult -- 呼叫 .ExecuteAsync()來建立HttpResponseMessage，然後將轉換為 HTTP 回應訊息。
+    //         ( .NET Core的 WebAPI有變化）
+    //
+    //  (4)  其他類型 -- 將 序列化 的傳回值寫入至回應主體中; 傳回 200 （確定）。
+    //                            缺點是您不能直接傳回錯誤碼 404 等。 不過，您可以擲回HttpResponseException錯誤代碼
+    //===========================================================
 }
---
+++ wwwroot/js/site.js
@@ -0,0 +1,148 @@
+﻿// 微軟範例下載 https://github.com/dotnet/AspNetCore.Docs/tree/main/aspnetcore/tutorials/first-web-api/samples
+// 官方說明文件 https://learn.microsoft.com/zh-tw/aspnet/core/tutorials/first-web-api?view=aspnetcore-7.0&tabs=visual-studio
+// 搭配 /Models目錄。請搭配 wwwroot目錄下的 index.html 來使用。     
+
+// <snippet_SiteJs>
+const uri = 'api/todoitems';   //******** 請修改成自己的 api網址 ***
+let todos = [];
+
+// 參考文章 https://www.oxxostudio.tw/articles/201908/js-fetch.html
+//fetch('網址')
+//    .then(function (response) {
+//        // 處理 response
+//    }).catch(function (err) {
+//        // 錯誤處理
+//    });
+// Fetch API的完整屬性說明，請看 https://developer.mozilla.org/en-US/docs/Web/API/Request
+
+function getItems() {
+// <snippet_GetItems>
+  fetch(uri)
+    .then(response => response.json())
+    .then(data => _displayItems(data))
+    .catch(error => console.error('Unable to get items.無法取得任何 items[待辦項目]', error));
+// </snippet_GetItems>
+}
+
+// <snippet_AddItem>
+function addItem() {
+  const addNameTextbox = document.getElementById('add-name');
+
+  const item = {
+    isComplete: false,
+    name: addNameTextbox.value.trim()
+  };
+
+  fetch(uri, {
+    method: 'POST',
+    headers: {
+      'Accept': 'application/json',
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify(item)
+  })
+    .then(response => response.json())
+    .then(() => {
+      getItems();   //重新整理畫面，取得最新的待辦事項，並呈現在畫面上。
+      addNameTextbox.value = '';
+    })
+    .catch(error => console.error('Unable to add item.無法新增[待辦項目]', error));
+}
+// </snippet_AddItem>
+
+function deleteItem(id) {
+  // <snippet_DeleteItem>
+  fetch(`${uri}/${id}`, {
+    method: 'DELETE'
+  })
+  .then(() => getItems())    //重新整理畫面，取得最新的待辦事項，並呈現在畫面上。
+      .catch(error => console.error('Unable to delete item.無法刪除[待辦項目]', error));
+  // </snippet_DeleteItem>
+}
+
+function displayEditForm(id) {
+  const item = todos.find(item => item.id === id);
+  
+  document.getElementById('edit-name').value = item.name;
+  document.getElementById('edit-id').value = item.id;
+  document.getElementById('edit-isComplete').checked = item.isComplete;
+  document.getElementById('editForm').style.display = 'block';
+}
+
+function updateItem() {
+  const itemId = document.getElementById('edit-id').value;
+  const item = {
+    id: parseInt(itemId, 10),
+    isComplete: document.getElementById('edit-isComplete').checked,
+    name: document.getElementById('edit-name').value.trim()
+  };
+
+  // <snippet_UpdateItem>
+  fetch(`${uri}/${itemId}`, {
+    method: 'PUT',
+    headers: {
+      'Accept': 'application/json',
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify(item)
+  })
+  .then(() => getItems())    //重新整理畫面，取得最新的待辦事項，並呈現在畫面上。
+      .catch(error => console.error('Unable to update item.無法更新[待辦項目]', error));
+  // </snippet_UpdateItem>
+
+  closeInput();
+
+  return false;
+}
+
+function closeInput() {
+  document.getElementById('editForm').style.display = 'none';
+}
+
+function _displayCount(itemCount) {
+  const name = (itemCount === 1) ? 'to-do' : 'to-dos';
+
+  document.getElementById('counter').innerText = `${itemCount} ${name}`;
+}
+
+function _displayItems(data) {
+  const tBody = document.getElementById('todos');
+  tBody.innerHTML = '';
+
+  _displayCount(data.length);
+
+  const button = document.createElement('button');
+
+  data.forEach(item => {
+    let isCompleteCheckbox = document.createElement('input');
+    isCompleteCheckbox.type = 'checkbox';
+    isCompleteCheckbox.disabled = true;
+    isCompleteCheckbox.checked = item.isComplete;
+
+    let editButton = button.cloneNode(false);
+    editButton.innerText = 'Edit編輯';
+    editButton.setAttribute('onclick', `displayEditForm(${item.id})`);
+
+    let deleteButton = button.cloneNode(false);
+    deleteButton.innerText = 'Delete刪除';
+    deleteButton.setAttribute('onclick', `deleteItem(${item.id})`);
+
+    let tr = tBody.insertRow();
+    
+    let td1 = tr.insertCell(0);
+    td1.appendChild(isCompleteCheckbox);
+
+    let td2 = tr.insertCell(1);
+    let textNode = document.createTextNode(item.name);
+    td2.appendChild(textNode);
+
+    let td3 = tr.insertCell(2);
+    td3.appendChild(editButton);
+
+    let td4 = tr.insertCell(3);
+    td4.appendChild(deleteButton);
+  });
+
+  todos = data;
+}
+// </snippet_SiteJs>
