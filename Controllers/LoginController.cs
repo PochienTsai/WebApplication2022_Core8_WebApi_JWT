c8dbdc44e4d92cf6603dd1c4ba9a9aba8802b1a4
PochienTsai <bernie7135@gmail.com>
2024-10-13 11:12:15 +0800

    Add LoginController and refactor HomeDBController.
    Introduce a new `LoginController` for handling login operations. Enhanced readability and consistency in `HomeDBController` by adjusting spacing, converting to file-scoped namespace, and improving comments.

---
+++ Controllers/LoginController.cs
@@ -0,0 +1,42 @@
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebApplication2022_Core8_WebApi_JWT.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class LoginController : ControllerBase
+    {
+        // GET: api/<LoginController>
+        [HttpGet]
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "value1", "value2" };
+        }
+
+        // GET api/<LoginController>/5
+        [HttpGet("{id}")]
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST api/<LoginController>
+        [HttpPost]
+        public void Post([FromBody] string value)
+        {
+        }
+
+        // PUT api/<LoginController>/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        // DELETE api/<LoginController>/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
